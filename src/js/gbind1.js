4e2f9c6fba10c5e9cde1d8810dc3b36ef0480dbd
Gireesh Kumar <gireeshkumar.g@gmail.com>
2013-03-22 17:34:02 +0530

    updated to use '__proto__'

--- src/js/gbind1.js
+++ src/js/gbind1.js
@@ -3,37 +3,39 @@
  */
 
 // object.watch
-if (!Object.prototype._watch) {
-	Object.prototype._watch = function(prop, handler) {
-		var val = this[prop], getter = function() {
-			return val;
-		}, setter = function(newval) {
-			val = newval;
-			return val = handler.call(this, prop, val, newval);
-		};
-		if (delete this[prop]) { // can't watch constants
-			if (Object.defineProperty) { // ECMAScript 5
-				Object.defineProperty(this, prop, {
-					get : getter,
-					set : setter
-				});
-			} else if (Object.prototype.__defineGetter__
-					&& Object.prototype.__defineSetter__) // legacy
-			{
-				Object.prototype.__defineGetter__.call(this, prop, getter);
-				Object.prototype.__defineSetter__.call(this, prop, setter);
+function checkAddPropertyWatch(objinstance){
+	if (!objinstance.__proto__.$gwatch) {
+		objinstance.__proto__.$gwatch = function(prop, handler) {
+			var val = this[prop], getter = function() {
+				return val;
+			}, setter = function(newval) {
+				val = newval;
+				return val = handler.call(this, prop, val, newval);
+			};
+			if (delete this[prop]) { // can't watch constants
+				if (objinstance.defineProperty) { // ECMAScript 5
+					objinstance.defineProperty(this, prop, {
+						get : getter,
+						set : setter
+					});
+				} else if (objinstance.__proto__.__defineGetter__
+						&& objinstance.__proto__.__defineSetter__) // legacy
+				{
+					objinstance.__proto__.__defineGetter__.call(this, prop, getter);
+					objinstance.__proto__.__defineSetter__.call(this, prop, setter);
+				}
 			}
-		}
-	};
-}
+		};
+	}
 
-// object.unwatch
-if (!Object.prototype._unwatch) {
-	Object.prototype._unwatch = function(prop) {
-		var val = this[prop];
-		delete this[prop]; // remove accessors
-		this[prop] = val;
-	};
+	// object.unwatch
+	if (!objinstance.__proto__.$gunwatch) {
+		objinstance.__proto__.$gunwatch = function(prop) {
+			var val = this[prop];
+			delete this[prop]; // remove accessors
+			this[prop] = val;
+		};
+	}
 }
 
 
@@ -332,7 +334,8 @@ $(function(){
 			  
 			  
 			  if( !(eventhandler[idname][propname])){
-				  window[idname]._watch(propname, function(){
+				  checkAddPropertyWatch(window[idname]);
+				  window[idname].$gwatch(propname, function(){
 					  $( window[idname] ).trigger("propertychange",{property:propname, val:window[idname][propname], obj:window[idname]});
 				  });
 				  eventhandler[idname][propname] = true;
